df7e7585b7ef2f27c60d076a217cf8f663f6ef8d
yyue <yy@moz0.com>
2021-02-01 08:37:53 +0000

    protoc exec success

--- protoc/generate.go
+++ protoc/generate.go
@@ -19,16 +19,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = filepath.Walk("./", func(p string, info os.FileInfo, err error) error {
+	err = filepath.Walk(dir, func(p string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		fmt.Println(p, info.IsDir(), filepath.Ext(p))
-		if !info.IsDir() && filepath.Ext(p) == ".proto" {
+		relPath, err := filepath.Rel(dir, p)
+		if err != nil {
+			return err
+		}
+		fmt.Println(relPath, relPath, info.IsDir(), filepath.Ext(relPath), filepath.Ext(relPath))
+		if filepath.Ext(relPath) == ".proto" {
 
 			args := []string{
-				"-I=.",
-				"-I=$GOPATH/src/github.com/googleapis/googleapis",
+				"--proto_path=./",
+				fmt.Sprintf("--proto_path=%s/.local/include", os.Getenv("HOME")),
+				fmt.Sprintf("--proto_path=%s/src/github.com/googleapis/googleapis", os.Getenv("GOPATH")),
 				// 分开写会生成两个文件, 下面的写一起只生成一个文件 ,
 				// 下面的import的context是golang.org/x/net/context, 以被内部context取代,
 				// 所以用上面的
@@ -39,13 +44,13 @@ func main() {
 				// js
 				// fmt.Sprintf("--js_out=import_style=commonjs,binary:%s", webOutPath),
 				// fmt.Sprintf("--grpc-web_out=import_style=typescript,mode=grpcwebtext:%s", webOutPath),
-				p,
+				relPath,
 			}
 			cmd := exec.Command("protoc", args...)
 			cmd.Dir = dir
 			out, err := cmd.CombinedOutput()
 			if err != nil {
-				fmt.Printf("protoc [%s] error: %s, out: \n%s\n%s\n", p, err, out, args)
+				fmt.Printf("protoc [%s] error: %s, out: \n%s\n%s\n", relPath, err, out, args)
 			} else {
 				fmt.Printf("protoc success: %s\n", p)
 			}
